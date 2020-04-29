4f621cd82b416eeef5179a7474e1684280d6b78b
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-04-29 16:53:49 +0530

    Initial commit

---
+++ src/Graph_using_BFS_Algorithm/GraphNode.java
@@ -0,0 +1,16 @@
+package Graph_using_BFS_Algorithm;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Abhinav Bhardwaj
+ * Date: 29-04-2020
+ * Time: 16:52
+ */
+
+public class GraphNode {
+    int value ;
+
+    public GraphNode(int value) {
+        this.value = value;
+    }
+}
---
+++ src/Graph_using_BFS_Algorithm/MyGraph.java
@@ -0,0 +1,11 @@
+package Graph_using_BFS_Algorithm;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Abhinav Bhardwaj
+ * Date: 29-04-2020
+ * Time: 16:52
+ */
+
+public class MyGraph {
+}
